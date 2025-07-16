303d94ca2637bee4624038de6c088016ee31af97
DasAneesh <149708161+DasAneesh@users.noreply.github.com>
2025-07-16 20:35:23 +0300

    Updates

--- Famiry-client/vite.config.ts
+++ Famiry-client/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0',  // Добавьте эту строку!
+    port: 5173,       // Используйте стандартный порт Vite
     proxy: {
       '/api': {
         target: 'http://famiry:8080',
